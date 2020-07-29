ff05fae4c4a87fbbe385fbcffbcccb74da6329e6
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-07-29 10:27:05 +0300

    downside-of-lifting-state-up

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProfileCard from '../components/ProfileCard';
 
-const UserPage = () => {
+
+const UserPage = props => {
     return (
         <div className="container">
-            userpage
+            <ProfileCard username={props.username} />
         </div>
     );
 };
