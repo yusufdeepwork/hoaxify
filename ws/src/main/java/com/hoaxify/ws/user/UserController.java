ae92afa735f9d8bc62d3c07a5ff2729961e48a8c
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-08-20 15:34:52 +0300

    pagination

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -8,6 +8,8 @@ import com.hoaxify.ws.shared.Views;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
@@ -27,9 +29,10 @@ public class UserController {
         return  new GenericResponse("user created");
     }
     @GetMapping("/api/1.0/users")
-    @JsonView(Views.Base.class)
-    List<User> getUsers(){
-        return userService.getUsers();
+    //@JsonView(Views.Base.class)
+    Page<User> getUsers(Pageable page) {
+        return userService.getUsers(page);
+
     }
 
 }
--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -2,6 +2,8 @@ package com.hoaxify.ws.user;
 
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,7 +28,7 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public Page<User> getUsers(Pageable page) {
+        return userRepository.findAll(page);
     }
 }
