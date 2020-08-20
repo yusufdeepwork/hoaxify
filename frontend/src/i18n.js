70baeafac367e34c3486833b7a88a1281dc50f8a
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-08-20 15:18:14 +0300

    user-list

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -17,7 +17,8 @@ i18n.use(initReactI18next).init({
                 Password : 'Password',
                 'Password Repeat' : 'Password Repeat',
                 'Login': 'Login',
-                'Logout':'Logout'
+                'Logout':'Logout',
+                'Users':'Users'
 
             }
 
@@ -33,7 +34,8 @@ i18n.use(initReactI18next).init({
                 Password : 'Şifre',
                 'Password Repeat' : 'Şifreyi Tekrarla',
                 'Login': 'Sisteme gir',
-                'Logout':'Çık'
+                'Logout':'Çık',
+                'Users':'Kullanıcılar'
             }
         }
     },
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import UserList from '../components/UserList';
 const HomePage = () => {
     return (
         <div className="container">
-            homepage
+           <UserList/>
         </div>
     );
 };
