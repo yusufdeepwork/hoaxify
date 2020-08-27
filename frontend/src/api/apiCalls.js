1c7170f3e8fa51ff4fc68c7262f467d5e846fb31
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-08-27 11:05:25 +0300

    load-user-data-in-user-page

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -23,5 +23,9 @@ export const setAuthorizationHeader = ({username,password,isLoggedIn}) => {
   }else{
     delete axios.defaults.headers['Authorization'];
   }
-
 }
+
+export const getUser = username => {
+ return axios.get(`/api/1.0/users/${username}`);
+};
+
