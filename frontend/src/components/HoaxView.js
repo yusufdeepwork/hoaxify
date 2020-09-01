deb516fedad77eddde93e4eafa9ca4f719754b64
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-01 17:25:31 +0300

    relative-time

--- frontend/src/components/HoaxView.js
+++ frontend/src/components/HoaxView.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { Link } from 'react-router-dom';
-
+import { useTranslation } from 'react-i18next';
+import {format} from 'timeago.js'
 const HoaxView = props => {
     const {hoax} = props;
-    const {user,content} =hoax;
+    const {user,content,timestamp} =hoax;
     const {username,displayName,image} =user;
+
+    const {i18n} = useTranslation()
+    const formatted = format(timestamp,i18n.language);
 
     return (
         <div className="card p-1">
@@ -13,15 +17,18 @@ const HoaxView = props => {
                 <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
                 <div className="flex-fill m-auto pl-2">
                     <Link to={`/user/${username}`} className="text-dark">
-                        <h6>
+                        <h6 className="d-inline">
                             {displayName}@{username}
                         </h6>
+                        <span> - </span>
+                        <span>{formatted}</span>
                     </Link>
                 </div>
             </div>
             <div className="pl-5">{content}</div>
         </div>
     );
+    
 };
 
 export default HoaxView;
