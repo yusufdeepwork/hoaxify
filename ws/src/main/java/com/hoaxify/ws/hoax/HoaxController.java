7b3b692be6f6e190e0c73f7e151b2e70a234a5b4
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-01 12:54:38 +0300

    Hoaxes-Sorting

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
@@ -4,6 +4,8 @@ import com.hoaxify.ws.shared.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -21,7 +23,7 @@ public class HoaxController {
         return new GenericResponse("Hoax is saved");
     }
     @GetMapping("/hoaxes")
-    Page<Hoax> getHoaxes (Pageable page){
+    Page<Hoax> getHoaxes (@PageableDefault(sort="id",direction = Sort.Direction.DESC) Pageable page){
         return hoaxService.getHoaxes(page);
     }
 }
