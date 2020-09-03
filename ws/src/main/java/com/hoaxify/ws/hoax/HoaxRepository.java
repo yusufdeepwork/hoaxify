53728cfd00febeffcd0c76d3031fbcf316b938e1
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-03 15:18:35 +0300

    refactor-with-specification

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxRepository.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxRepository.java
@@ -5,23 +5,12 @@ import com.hoaxify.ws.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface HoaxRepository extends JpaRepository<Hoax,Long> {
-
+public interface HoaxRepository extends JpaRepository<Hoax,Long>, JpaSpecificationExecutor<Hoax> {
     Page<Hoax> findByUser(User inDB, Pageable page);
-
-    Page<Hoax> findByIdLessThan(long id, Pageable page);
-
-    Page<Hoax> findByIdLessThanAndUser(long id, User user, Pageable page);
-
-    long countByIdGreaterThan(long id);
-
-    long countByIdGreaterThanAndUser(long id, User user);
-
-    List<Hoax> findByIdGreaterThan(long id, Sort sort);
-
-    List<Hoax> findByIdGreaterThanAndUser(long id, User user, Sort sort);
 }
