57af46152b9501dca81ddd4a172cffcd8c6919b3
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-03 20:28:28 +0300

    fixed-2

--- frontend/src/components/HoaxSubmit.js
+++ frontend/src/components/HoaxSubmit.js
@@ -21,6 +21,9 @@ const HoaxSubmit = () => {
             setHoax('');
             setErrors({});
             setNewImage();
+           
+            setAttachmentId();
+
         }
     },[focused]);
 
