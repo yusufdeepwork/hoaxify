c0f350e27b3bb2dcd1547a31df05557339d2acb2
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-03 19:24:44 +0300

    scheduled-service

--- ws/src/main/java/com/hoaxify/ws/file/FileAttachmentRepository.java
+++ ws/src/main/java/com/hoaxify/ws/file/FileAttachmentRepository.java
@@ -2,6 +2,10 @@ package com.hoaxify.ws.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long>{
 
+    List<FileAttachment> findByDateBeforeAndHoaxIsNull(Date date);
 }
--- ws/src/main/java/com/hoaxify/ws/file/FileService.java
+++ ws/src/main/java/com/hoaxify/ws/file/FileService.java
@@ -3,6 +3,8 @@ package com.hoaxify.ws.file;
 
 import com.hoaxify.ws.configuration.AppConfiguration;
 import org.apache.tika.Tika;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,10 +16,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 
 @Service
+@EnableScheduling
 public class FileService {
 
     AppConfiguration appConfiguration;
@@ -77,4 +81,16 @@ public class FileService {
         attachment.setDate(new Date());
         return fileAttachmentRepository.save(attachment);
     }
+
+
+    @Scheduled(fixedRate = 24*60*60*1000)
+    public void cleanupStorage(){
+        Date twentyFourHoursAgo = new Date(System.currentTimeMillis() - (24*60*60*1000));
+        List<FileAttachment> filesToBeDeleted = fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(twentyFourHoursAgo);
+        for (FileAttachment file : filesToBeDeleted){
+            deleteFile(file.getName());
+            fileAttachmentRepository.deleteById(file.getId());
+        }
+    }
+
 }
